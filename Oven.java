6ee7bcb06df311e47e2ac113dd002df3ee438a26
badboy53 <72854526+badboy53@users.noreply.github.com>
2022-11-28 12:56:42 +0200

    Add files via upload

---
+++ Oven.java
@@ -0,0 +1,37 @@
+public class Oven extends Device{
+    private int tps;
+    private double tf;
+    private String diak;
+    private String pso;
+    private String sk;
+    static int countOv;
+
+
+    /** prosorina adio function */
+    //private String periodicMaintanceOv(){}
+
+    /** constractor public
+     * @param tps   ARITHMOS TROPOY PSISIMATOS
+     * @param tf    XORITIKOTITA FOURNOU
+     * @param diak  DIAKOPTES
+     * @param pso   PSIFIAKI OTHONI
+     * @param sk    SISTIMA KATHARISMOY
+     * */
+    public Oven(double m, double h, double d, String ak, String as, String ts, double ke, int tps, double tf, String diak, String pso, String sk) {
+        super(m, h,d, ak, as, ts, ke);
+        this.tps = tps;
+        this.tf = tf;
+        this.diak = diak;
+        this.pso = pso;
+        this.sk = sk;
+        countOv++;
+    }
+
+    /**
+     * toString method public
+     * return
+     * */
+    public String toString(){
+        return tps + ", " + tf + ", " + diak + ", " + pso + ", " + sk + '\n' + countOv + '\n';
+    }
+}
